7c4fcbbe8a80154dafaea4c55ff0db2c818a43f6
Roman Minkin <rminkin@vaynermedia.com>
2014-03-05 18:22:25 -0500

    Ahhhhh, typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -487,7 +487,7 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('perform-release', function() {
-        grunt.task.requires(['prepare-release', 'dist']);
+        grunt.task.requires(['prepare-release', 'build']);
 
         var version = grunt.config('pkg.version'),
             releasedir = grunt.config('dirs.build');
